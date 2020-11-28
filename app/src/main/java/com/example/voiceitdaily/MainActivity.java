385ea82ab6b7750ac0d680eea304cc794ed6ddd6
vignesh98 <vigneshn000@gmail.com>
2020-11-28 18:21:15 +0530

    First commit, first activity with record and history buttons

--- app/src/main/java/com/example/voiceitdaily/MainActivity.java
+++ app/src/main/java/com/example/voiceitdaily/MainActivity.java
@@ -1,14 +1,35 @@
-fpackage com.example.voiceitdaily;
+package com.example.voiceitdaily;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView dateTimeDisplay;
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String date;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting date in the textview
+
+        dateTimeDisplay = (TextView)findViewById(R.id.todays_date);
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("E, MMM d, ''yy");
+        date = dateFormat.format(calendar.getTime());
+        dateTimeDisplay.setText(date);
+
+
     }
 }
