95fd5e4f01eda45077f01991209962d14331ad1d
vignesh98 <vigneshn000@gmail.com>
2020-11-28 17:05:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VoiceitDaily
+ A new app to record your day in voice and live translate the same into a text to save it into a pdf
---
+++ app/src/main/java/com/example/voiceitdaily/MainActivity.java
@@ -0,0 +1,14 @@
+fpackage com.example.voiceitdaily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
